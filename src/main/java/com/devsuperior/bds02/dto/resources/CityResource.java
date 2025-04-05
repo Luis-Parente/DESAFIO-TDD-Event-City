04d84e0b3edfff4c4e10dd36682d6df6b45816a9
Luis Parente <lgsp25@hotmail.com>
2025-04-05 10:02:57 -0300

    CityControllerIT second test ok

--- src/main/java/com/devsuperior/bds02/dto/resources/CityResource.java
+++ src/main/java/com/devsuperior/bds02/dto/resources/CityResource.java
@@ -1,12 +1,16 @@
 package com.devsuperior.bds02.dto.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
@@ -14,7 +18,7 @@ import com.devsuperior.bds02.services.CityService;
 @Controller
 @RequestMapping(value = "/cities")
 public class CityResource {
-	
+
 	@Autowired
 	private CityService service;
 
@@ -22,6 +26,13 @@ public class CityResource {
 	public ResponseEntity<List<CityDTO>> findAll() {
 		List<CityDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+
+	@PostMapping
+	public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
@@ -19,5 +20,15 @@ public class CityService {
 	public List<CityDTO> findAll() {
 		List<City> result = repository.findAll(Sort.by("name"));
 		return result.stream().map(x -> new CityDTO(x)).toList();
+	}
+
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+
+		return new CityDTO(entity);
 	}
 }
